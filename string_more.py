c0949226a552983e7ac1cd9359f45028b46a3e54
prubac <prubac@sgh.waw.pl>
2025-10-20 12:37:47 +0200

    Playing with strings, split, ord, chr

---
+++ string_more.py
@@ -0,0 +1,21 @@
+s1 = 'booker12\t9012\tRachel\tBooker'
+
+ar1 = [ 131, 1542, 63645 ]
+print(ar1)
+print(ar1[0])
+print(ar1[1])
+
+ars1 = s1.split('\t')
+print(ars1)
+
+s2 = ' | '.join(ars1)
+print(s2)
+
+print('-----------------')
+
+print(ord('a'))
+print(ord('A'))
+print(ord('1'))
+print(ord('ż'))
+
+print(chr(98))

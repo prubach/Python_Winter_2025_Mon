37a53a047ff5f58429f638389bca3918054f3c9e
prubac <prubac@sgh.waw.pl>
2025-10-20 14:57:33 +0200

    2D list exercise

---
+++ lists_2D.py
@@ -0,0 +1,14 @@
+num_list = [(2, 4), (4, 7), (5, 6), (6, 8), (7, 9), (8, 10)]
+for el in num_list:
+    print(f'{el[0]}  {el[1]}')
+
+
+list_2D = [[2, 4, 6], [4, 7, 8], [5, 6, 9], [6, 8, 10]]
+
+print(list_2D[1])
+print(list_2D[1][1])
+
+# 1. Sum each row
+# 2. Sum each column
+# 3. Sum all elements of list_2D
+
--- play_with_numbers.py
+++ play_with_numbers.py
@@ -1,3 +1,8 @@
 l1 = [1, 2, 4, 7, 9, 10, 524, 46, 65]
 # Create a list containing all second powers of the elements from l1
 # Sum all those second powers of l1
+
+l1_2nd_powers = [x**2 for x in l1]
+print(l1)
+print(l1_2nd_powers)
+print(sum(l1_2nd_powers))

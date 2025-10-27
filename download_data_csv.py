7df635b99893026d8da641abedc40af4caa4bf2a
prubac <prubac@sgh.waw.pl>
2025-10-27 13:12:32 +0100

    Download data from Stooq

--- download_data_csv.py
+++ download_data_csv.py
@@ -6,5 +6,8 @@ local_path = 'data/SPX.csv'
 with urlopen(url) as webcontent, open(local_path, 'wb') as local_file:
     spx_content = webcontent.read()
     lines = spx_content.splitlines()
-    print(lines[:2])
+    lines_str = []
+    for line in lines:
+        lines_str.append(line.decode('utf8'))
+    print(lines_str[:2])
     local_file.write(spx_content)

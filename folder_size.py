c437172c292ef88d078e1eed04cb2ff03ee687af
prubac <prubac@sgh.waw.pl>
2025-10-27 14:42:59 +0100

    Folder size exercise

---
+++ folder_size.py
@@ -0,0 +1,12 @@
+import os
+
+my_folder = 'data'
+
+# Given the folder above please sum up the space occupied by all files and folders inside it.
+
+os.listdir(my_folder) # List files/folders inside a folder
+
+os.path.isdir(my_folder) # Check if it is a folder
+os.path.isfile(my_file)  # Check if it is a regular file
+os.path.getsize(my_file) # Get the size of a file
+

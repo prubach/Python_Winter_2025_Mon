3a0b9ef36cd068ac2042a9d56877c6afa9861e09
prubac <prubac@sgh.waw.pl>
2025-10-27 13:03:31 +0100

    Download data from Stooq

---
+++ download_data_csv.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen
+
+url = 'https://stooq.com/q/d/l/?s=^spx&i=d'
+
+local_path = 'data/SPX.csv'
+with urlopen(url) as webcontent, open(local_path, 'wb') as local_file:
+    spx_content = webcontent.read()
+    lines = spx_content.splitlines()
+    print(lines[:2])
+    local_file.write(spx_content)

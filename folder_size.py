7cb84847361522522d27bce478edd20a43c040c6
prubac <prubac@sgh.waw.pl>
2025-11-03 14:12:32 +0100

    Compute folder size using recursion

--- folder_size.py
+++ folder_size.py
@@ -8,6 +8,19 @@ os.listdir(my_folder) # List files/folders inside a folder
 
 os.path.isdir(my_folder) # Check if it is a folder
 #os.path.isfile(my_file)  # Check if it is a regular file
-#os.path.getsize(my_file) # Get the size of a file
+fold_size = os.path.getsize(my_folder) # Get the size of a file
+#print(fold_size)
 
-print(os.path.getsize(my_folder))
+#print(os.path.getsize(my_folder))
+
+
+def space_occupied(my_file):
+    if os.path.isfile(my_file):
+        return os.path.getsize(my_file)
+    if os.path.isdir(my_file):
+        space_count = 0
+        for f in os.listdir(my_file):
+            space_count += space_occupied(os.path.join(my_file, f))
+        return space_count
+
+print(space_occupied(my_folder))

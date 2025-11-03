8edb4f7062e970e16242c6932a692c2f74fb2d2b
Paweł Rubach <pawel.rubach@gmail.com>
2025-11-03 11:56:46 +0100

    Bowls count

---
+++ bowling_game.py
@@ -0,0 +1,16 @@
+"""
+n  - number of rows
+
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+
+How many bowls do we need given n - the number of rows?
+
+"""
+
+def bowls_count(n):
+    return

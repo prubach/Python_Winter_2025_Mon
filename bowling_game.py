4a8d5f726a7470a9d67d031963e1ce77feb479ab
prubac <prubac@sgh.waw.pl>
2025-11-03 13:05:13 +0100

    Performance test of bowls counting functions

--- bowling_game.py
+++ bowling_game.py
@@ -34,12 +34,12 @@ def bowls_sum_recursive(n):
         return bowls_sum_recursive(n - 1) + n
     return s
 
+if __name__ == '__main__':
+    print(f'for n=5 using seq: {bowls_sum_seq(5)}')
+    print(f'for n=6 using seq: {bowls_sum_seq(6)}')
 
-print(f'for n=5 using seq: {bowls_sum_seq(5)}')
-print(f'for n=6 using seq: {bowls_sum_seq(6)}')
+    print(f'for n=5 using loop: {bowls_sum_loop(5)}')
+    print(f'for n=6 using loop: {bowls_sum_loop(6)}')
 
-print(f'for n=5 using loop: {bowls_sum_loop(5)}')
-print(f'for n=6 using loop: {bowls_sum_loop(6)}')
-
-print(f'for n=5 using recursive: {bowls_sum_recursive(5)}')
-print(f'for n=6 using recursive: {bowls_sum_recursive(6)}')
+    print(f'for n=5 using recursive: {bowls_sum_recursive(5)}')
+    print(f'for n=6 using recursive: {bowls_sum_recursive(6)}')
--- performance_test.py
+++ performance_test.py
@@ -1,5 +1,5 @@
 from time import time
-
+from bowling_game import bowls_sum_loop, bowls_sum_recursive, bowls_sum_seq
 
 def time_func(func, n):
     t0 = time()
@@ -9,3 +9,9 @@ def time_func(func, n):
     t1 = time()
     elapsed = round(t1 - t0, 8)
     print(f'Done in {elapsed} sec')
+
+#n = 998
+n = 25000000
+#time_func(bowls_sum_recursive, n)
+time_func(bowls_sum_loop, n)
+time_func(bowls_sum_seq, n)

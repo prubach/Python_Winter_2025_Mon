c483c288e30605cca932c2d70ff43ff63478c443
prubac <prubac@sgh.waw.pl>
2025-11-17 14:09:40 +0100

    Bank - working on classes

---
+++ bank.py
@@ -0,0 +1,35 @@
+class Customer:
+    last_id = 0
+
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        Customer.last_id += 1
+        self.id = Customer.last_id
+
+    def __repr__(self):
+        return f'Customer[{self.id}, {self.firstname}, {self.lastname}]'
+
+class Account:
+    last_id = 1000
+
+    def __init__(self, customer):
+        self.customer = customer
+        Account.last_id += 1
+        self.id = Account.last_id
+        self._balance = 0.0
+
+    def __repr__(self):
+        return f'Account[{self.id}, {self.customer.lastname}, {self._balance}]'
+
+
+
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.customer_list = []
+        self.account_list = []
+
+    def create_customer(self, firstname, lastname):
+
+
